43e02f2d4e6b9b5da88fcbe41611afcadc8b0fcf
Aditiya Karthikeyan <wolverineadi19@gmail.com>
2025-08-15 20:42:19 +0530

    Add files via upload

---
+++ Breadth_First_Search.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <queue>
+#include <string>
+#include <set>
+
+using namespace std;
+
+unordered_map<char, vector<char>> ntwrk;
+
+string breadthFirstSearch(char node, char trgt) {
+    queue<string> routeQueue;
+    set<char> visited;
+
+    routeQueue.push(string(1, node));
+    visited.insert(node);
+
+    while (!routeQueue.empty()) {
+        string currPath = routeQueue.front();
+        routeQueue.pop();
+
+        char lastnode = currPath.back();
+
+        if (lastnode == trgt) {
+            return currPath;
+        }
+
+        for (char nghbr : ntwrk[lastnode]) {
+            if (!visited.count(nghbr)) {
+                visited.insert(nghbr);
+                routeQueue.push(currPath + nghbr);
+            }
+        }
+    }
+
+    return "";
+}
+
+int main() {
+    ntwrk['S'] = {'A', 'B'};
+    ntwrk['A'] = {'D'};
+    ntwrk['B'] = {'A', 'C'};
+    ntwrk['C'] = {'E'};
+    ntwrk['D'] = {'G'};
+    ntwrk['E'] = {};
+    ntwrk['G'] = {};
+
+    char origin = 'S';
+    char dstn = 'G';
+
+    string shortestPath = breadthFirstSearch(origin, dstn);
+
+    if (!shortestPath.empty()) {
+        cout << "Shortest path: " << shortestPath << "\n";
+    } else {
+        cout << "No route available.\n";
+    }
+
+    return 0;
+}
